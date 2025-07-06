02563bd58f88ab6a7d6b616fd3843e05e45aaca4
afrodemo <hasaniymkindi@gmail.com>
2025-07-06 13:27:01 +0300

    go go bang

---
+++ public/build/assets/user-BeBrnzWe.js
@@ -0,0 +1,11 @@
+import{c}from"./createLucideIcon-CFrv8MY5.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]],t=c("CircleCheckBig",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],r=c("User",o);export{t as C,r as U};
